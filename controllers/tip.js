5a948fafc0a7cda5d26d43531e805248f2ce633a
Luis Garcia <luis.garcia.olivares@alumnos.upm.es>
2018-05-23 22:04:23 +0200

    Practica 7 v6

--- controllers/tip.js
+++ controllers/tip.js
@@ -25,8 +25,7 @@ exports.create = (req, res, next) => {
         {
             text: req.body.text,
             quizId: req.quiz.id,
-            //authorId: req.session.user ? req.session.user.id : 0
-            authorId: req.session.user && req.session.user.id || 0
+            authorId: req.session.user ? req.session.user.id : 0
         });
 
     tip.save()
